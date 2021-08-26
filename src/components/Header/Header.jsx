deec33ef5ec69cf3756678044b70e725128ab23a
bokiv <borislavivanisevic@gmail.com>
2021-08-26 23:25:18 +0200

    commit 26,08,

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,8 @@ export const Header = ({
   onLayoutToggle,
   isRefresh,
   onBtnRefresh,
+  onTyping,
+  search
 }) => {
   console.log("Header: ", isRefresh);
 
@@ -12,26 +14,33 @@ export const Header = ({
   //   window.location.reload(onBtnRefresh);
   // };
   return (
-    <nav>
+    <nav style={{minHeight: "118px"}}>
       <div className="nav-wrapper">
         <a href="#!" className="brand-logo center">
           React Users
         </a>
         <a
           href="#!"
-          className="waves-effect waves-light btn"
-          onClick={onLayoutToggle}
-        >
+          className="waves-effect waves-light btn"onClick={onLayoutToggle}>
           {" "}
           {isListView ? "List View" : "Grid View"}{" "}
         </a>
         <a
           href="#!"
-          className="waves-effect waves-light btn"
-          onClick={onBtnRefresh}
-        >
+          className="waves-effect waves-light btn"onClick={onBtnRefresh}>
           Refresh
         </a>
+        <div className="center row">
+            <div className="col s12 " >
+              <div className="row" id="topbarsearch">
+                <div className="input-field col s6 s12 red-text">
+                            {/* <i className="red-text material-icons prefix">search</i> */}
+<input type="text" placeholder="search" id="autocomplete-input" className="autocomplete white-text" 
+onChange={onTyping}  value={search} />
+                  </div>
+                </div>
+              </div>
+            </div> 
         
       </div>
      
